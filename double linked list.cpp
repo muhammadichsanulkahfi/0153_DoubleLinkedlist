64b219d37e1f675c6754ada32b9e5092067479fc
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-15 15:34:44 +0700

    comit2

--- double linked list.cpp
+++ double linked list.cpp
@@ -10,3 +10,57 @@ class Node
     Node *prev;
 };
 
+class DoubleLinkedList
+{
+    private:
+    Node *START;
+
+    public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        //step 1: Allocate memory for new node
+        Node * newNode = new Node();
+
+        //step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
+        //step 3: Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            //step 4: newNode.next = START
+            newNode->next = START;
+
+            //step 5: START.prev = newNode (if START exists)
+            if (START != NULL)
+            START->prev = newNode;
+
+            //step 6: newNode,prev = NULL
+            newNode->prev == NULL;
+
+            //step 7: START =newNode
+            START = newNode;
+            return;
+        }
+
+        //insert in between node
+        //step 8: Locate position for inserttion
+        Node *Current = START;
+        while (Current->next != NULL && Current->next->noMhs < nim)
+
+    }
+
+}
